aaafc6f35fc3cff5e6e0693db38b453ad1c86204
joshuaphilips <philipsjoshua96@gmail.com>
2021-11-02 13:43:45 +0000

    Topic controller request

---
+++ src/main/java/io/jphilips/springbootstarter/topic/TopicController.java
@@ -0,0 +1,18 @@
+package io.jphilips.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(new Topic("spring", "Spring Framework", "Spring framework description"),
+				new Topic("java", "Core Java", "Core java description"),
+				new Topic("javascript", "JavaScript", "JavaScript description"));
+	}
+}
