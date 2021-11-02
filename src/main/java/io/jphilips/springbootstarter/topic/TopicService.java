2795be2425b71ef920dbcf7dfefb4f8ceff2cab5
joshuaphilips <philipsjoshua96@gmail.com>
2021-11-02 16:42:57 +0000

    creating resource using post request

--- src/main/java/io/jphilips/springbootstarter/topic/TopicService.java
+++ src/main/java/io/jphilips/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.jphilips.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,9 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring framework description"),
-			new Topic("java", "Core Java", "Core java description"),
-			new Topic("javascript", "JavaScript", "JavaScript description"));
+	private List<Topic> topics = new ArrayList<Topic>(
+			Arrays.asList(new Topic("spring", "Spring Framework", "Spring framework description"),
+					new Topic("java", "Core Java", "Core java description"),
+					new Topic("javascript", "JavaScript", "JavaScript description")));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -17,5 +19,9 @@ public class TopicService {
 
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
